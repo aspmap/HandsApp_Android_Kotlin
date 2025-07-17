330dbc128416444580d7953cf286aace7754e340
aspmap <aspmap@yandex.ru>
2025-07-17 21:45:04 +0300

    Рефакторинг

---
+++ app/src/main/java/run/itlife/handsapp/ui/fragments/DialogsFragment.kt
@@ -0,0 +1,12 @@
+package run.itlife.handsapp.ui.fragments
+
+import run.itlife.handsapp.R
+
+
+class DialogsFragment : BaseFragment(R.layout.fragment_dialogs) {
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+}
---
+++ app/src/main/java/run/itlife/handsapp/ui/fragments/PostsFragment.kt
@@ -0,0 +1,10 @@
+package run.itlife.handsapp.ui.fragments
+
+import run.itlife.handsapp.R
+
+class PostsFragment : BaseFragment(R.layout.fragment_posts) {
+
+    override fun onResume() {
+        super.onResume()
+    }
+}

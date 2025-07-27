1ec8e7155fcbee59137612da19d15e0c2c7c003a
aspmap <aspmap@yandex.ru>
2025-07-27 03:11:44 +0300

    Создание контекстного меню в Профиле

--- app/src/main/java/run/itlife/handsapp/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/run/itlife/handsapp/ui/fragments/ProfileFragment.kt
@@ -1,10 +1,17 @@
 package run.itlife.handsapp.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import run.itlife.handsapp.R
 
 class ProfileFragment : BaseFragment(R.layout.fragment_profile) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
     }
 }

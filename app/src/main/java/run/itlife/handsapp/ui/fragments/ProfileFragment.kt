123715d56b30d2b5f1113a061c2f0748b1320da8
aspmap <aspmap@yandex.ru>
2025-08-10 00:33:41 +0300

    Рефакторинг функции работы с картинкой

--- app/src/main/java/run/itlife/handsapp/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/run/itlife/handsapp/ui/fragments/ProfileFragment.kt
@@ -25,6 +25,7 @@ class ProfileFragment : BaseFragment(R.layout.fragment_profile) {
         settings_btn_change_username.setOnClickListener { replaceFragment(ChangeUsernameFragment()) }
         settings_btn_change_bio.setOnClickListener { replaceFragment(ChangeBioFragment()) }
         //settings_change_photo.setOnClickListener { changePhotoUser() }
+        //settings_user_photo.downloadAndSetImage(USER.photoUrl)
         settings_change_photo.setOnClickListener { showToast(getString(R.string.info_photo_profile)) }
     }
 
@@ -57,21 +58,12 @@ class ProfileFragment : BaseFragment(R.layout.fragment_profile) {
             && resultCode == RESULT_OK && data != null) {
             val uri = CropImage.getActivityResult(data).uri
             val path = REF_STORAGE_ROOT.child(FOLDER_PROFILE_IMAGE).child(CURRENT_UID)
-            path.putFile(uri).addOnCompleteListener { task1 ->
-                if (task1.isSuccessful) {
-                    path.downloadUrl.addOnCompleteListener { task2 ->
-                        if (task2.isSuccessful) {
-                            val photoUrl = task2.result.toString()
-                            REF_DATABASE_ROOT.child(NODE_USERS).child(CURRENT_UID)
-                                .child(CHILD_PHOTO_URL).setValue(photoUrl)
-                                .addOnCompleteListener {
-                                    if (it.isSuccessful) {
-                                        settings_user_photo.downloadAndSetImage(photoUrl)
-                                        showToast(getString(R.string.toast_data_update))
-                                        USER.photoUrl = photoUrl
-                                    }
-                                }
-                        }
+            putImageToStorage(uri, path) {
+                getUrlFromStorage(path) {
+                    putUrlToDatabase(it) {
+                        settings_user_photo.downloadAndSetImage(it)
+                        showToast(getString(R.string.toast_data_update))
+                        USER.photoUrl = it
                     }
                 }
             }

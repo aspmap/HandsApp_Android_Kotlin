145caa48a2d6d704d89b7a150392e602a82228ac
aspmap <aspmap@yandex.ru>
2025-08-10 00:33:32 +0300

    Рефакторинг функции работы с картинкой

--- app/src/main/java/run/itlife/handsapp/utils/appDatabaseHelper.kt
+++ app/src/main/java/run/itlife/handsapp/utils/appDatabaseHelper.kt
@@ -30,3 +30,35 @@ fun initFirebase() {
     USER = User()
     CURRENT_UID = AUTH.currentUser?.uid.toString()
 }
+
+/*
+inline fun putUrlToDatabase(url: String, crossinline function: () -> Unit) {
+    REF_DATABASE_ROOT.child(NODE_USERS).child(CURRENT_UID)
+        .child(CHILD_PHOTO_URL).setValue(url)
+        .addOnSuccessListener {
+            function()
+        }
+        .addOnFailureListener {
+            showToast(it.message.toString())
+        }
+}
+
+inline fun getUrlFromStorage(path: StorageReference, crossinline function: (url: String) -> Unit) {
+    path.downloadUrl
+        .addOnSuccessListener {
+            function(it.toString())
+        }
+        .addOnFailureListener {
+            showToast(it.message.toString())
+        }
+}
+
+inline fun putImageToStorage(uri: Uri, path: StorageReference, crossinline function: () -> Unit) {
+    path.putFile(uri)
+        .addOnSuccessListener {
+            function()
+        }
+        .addOnFailureListener {
+            showToast(it.message.toString())
+        }
+}*/
